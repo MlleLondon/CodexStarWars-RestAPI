631840f9aeae6d8dfc8bae91516989e736df3f98
MlleLondon <garancedugimont@gmail.com>
2024-04-09 11:08:53 +0200

    RESTAPI - PHP

---
+++ server/bdd.php
@@ -0,0 +1,7 @@
+<?php
+$server = "localhost:3306";
+$username = "root";
+$password = "";
+$db = "codexStarWars";
+$conn = mysqli_connect($server, $username, $password, $db);
+?>
---
+++ server/dao.php
@@ -0,0 +1,102 @@
+<?php
+class DAO {
+	private $conn;
+	private $resource;
+
+    public function __construct($bdd, $resource) {
+        $this->conn = $bdd;
+        $this->resource= $resource;
+    }
+
+    public function get($id=null){
+        $query = "SELECT * FROM ".$this->resource. ($id ? " where id=".$id : "");
+        $response = array();
+        $result = mysqli_query($this->conn, $query);
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+		{
+            $row = array_map(function($value){
+                if(is_string($value))return ($value);
+                else return $value;
+            },$row);
+            $response[] = $row;
+        }
+        $response = ($id ? $response[0] : $response); 
+        header('Content-Type: application/json');
+		echo json_encode($response, JSON_PRETTY_PRINT);
+    }
+
+
+    public function add(){
+        $query="INSERT INTO ".$this->resource."(".implode(",", array_keys($_POST)).") ";
+        $query.= " VALUES(";
+        foreach($_POST as $value){
+            $query.= "'".$value."', ";
+        }
+        $query= substr($query, 0, -2).")";
+        if(mysqli_query($this->conn, $query))
+		{
+			$response=array(
+				'status' => 1,
+				'status_message' =>'Ressource ajouté avec succés.'
+			);
+		}
+		else
+		{
+			$response=array(
+				'status' => 0,
+				'status_message' =>'ERREUR!.'. mysqli_error($this->conn)
+			);
+		}
+		header('Content-Type: application/json');
+		echo json_encode($response);
+    }
+
+    public function update($id){
+        $_PUT = array();
+		parse_str(file_get_contents('php://input'), $_PUT);
+        $query="UPDATE ".$this->resource." SET ";
+        foreach($_PUT as $key=>$value){
+            $query.= $key."='".$value."', ";
+        }
+        $query= substr($query, 0, -2)." WHERE id=".$id;
+        if(mysqli_query($this->conn, $query))
+		{
+			$response=array(
+				'status' => 1,
+				'status_message' =>'Ressource mis a jour avec succes.'
+			);
+		}
+		else
+		{
+			$response=array(
+				'status' => 0,
+				'status_message' =>'Echec de la mise a jour de la ressource. '. mysqli_error($this->conn)
+			);
+			
+		}
+		
+		header('Content-Type: application/json');
+		echo json_encode($response);
+    }
+
+    public function delete($id){
+        $query = "DELETE FROM ".$this->resource." WHERE id=".$id;
+		if(mysqli_query($this->conn, $query))
+		{
+			$response=array(
+				'status' => 1,
+				'status_message' =>'Ressource supprimé avec succés.'
+			);
+		}
+		else
+		{
+			$response=array(
+				'status' => 0,
+				'status_message' =>'La suppression de la ressource a échoué.'. mysqli_error($this->conn)
+			);
+		}
+		header('Content-Type: application/json');
+		echo json_encode($response);
+    }
+}
+?>
