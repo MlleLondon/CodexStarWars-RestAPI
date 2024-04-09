3ead41068b97029fc9dbc714b994acde46bd56df
MlleLondon <garancedugimont@gmail.com>
2024-04-09 09:43:27 +0200

    Début

---
+++ src/main/java/com/example/codexstarwars/CodexstarwarsApplication.java
@@ -0,0 +1,13 @@
+package com.example.codexstarwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexstarwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodexstarwarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codexstarwars/CodexstarwarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codexstarwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodexstarwarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
