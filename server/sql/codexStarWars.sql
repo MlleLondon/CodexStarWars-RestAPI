284f2735e88f7a24764c52820584a322abf4b078
MlleLondon <garancedugimont@gmail.com>
2024-04-10 11:27:01 +0200

    Ajout personnages

--- server/sql/codexStarWars.sql
+++ server/sql/codexStarWars.sql
@@ -1,3 +1,7 @@
+drop database if exists codexStarWars;
+create database codexStarWars;
+use codexStarWars;
+
 CREATE TABLE planete(
     id INT NOT NULL AUTO_INCREMENT,
     nom VARCHAR(75) NOT NULL,
@@ -8,6 +12,21 @@ CREATE TABLE planete(
     type VARCHAR(75),
     diametre VARCHAR(75),
     population VARCHAR(75) NOT NULL,
+    image TEXT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE personnage(
+    id INT NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(75) NOT NULL,
+    description TEXT NOT NULL,
+    alias VARCHAR(75),
+    dateNaissance VARCHAR(75),
+    lieuNaissance VARCHAR(75),
+    dateMort VARCHAR(75),
+    lieuMort VARCHAR(75),
+    espece VARCHAR(75),
+    genre VARCHAR(75),
     image TEXT NOT NULL,
     PRIMARY KEY(id)
 );
